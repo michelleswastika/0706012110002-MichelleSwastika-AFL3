c31f43e48e897d7191b9e5e079f14cbe0e32fd92
michelleswastika <mswastika@student.ciputra.ac.id>
2023-04-11 08:39:14 +0700

    section 2

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, Michelle!")
+            Text("Turtle Rock")
+                .font(.title)
+                
         }
         .padding()
     }
