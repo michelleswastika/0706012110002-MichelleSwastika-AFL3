c25a28e5fe232a06c0d74c9465ea1fdc69261809
michelleswastika <mswastika@student.ciputra.ac.id>
2023-04-14 09:00:56 +0700

    section 2

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,32 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Michelle Swastika on 14/04/23.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+//  Add landmark as a stored property of LandmarkRow.
+    var landmark: Landmark
+    
+    var body: some View {
+//  Embed the existing text view in an HStack.
+        HStack {
+//  Complete the row by adding an image before the text view, and a spacer after it.
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+//  Modify the text view to use the landmark property’s name.
+            Text(landmark.name)
+        }
+    }
+}
+
+// In the previews static property of LandmarkRow_Previews, add the landmark parameter to the LandmarkRow initializer, specifying the first element of the landmarks array.
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+    }
+}
