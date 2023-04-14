a2488d9773076d32366503b02549a97e9f77ecf7
michelleswastika <mswastika@student.ciputra.ac.id>
2023-04-14 09:15:34 +0700

    section 6 completed

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
+//  Embed the dynamically generated list of landmarks in a NavigationView.
+        NavigationView {
 //  Replace the default Text view with a List, pass the model data’s landmarks array to the List initializer, return a LandmarkRow from the closure.
-        List(landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
+            List(landmarks) { landmark in
+//  Inside the list’s closure, wrap the returned row in a NavigationLink, specifying the LandmarkDetail view as the destination.
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+//  Call the navigationTitle(_:) modifier method to set the title of the navigation bar when displaying the list.
+            .navigationTitle("Landmarks")
         }
     }
 }
