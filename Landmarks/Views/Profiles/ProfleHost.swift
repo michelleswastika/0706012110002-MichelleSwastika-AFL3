8530ad0d90fdb85e9465e9f76d8dd50608d8aa5f
michelleswastika <mswastika@student.ciputra.ac.id>
2023-04-18 09:00:27 +0700

    chapter 3 completed

---
+++ Landmarks/Views/Profiles/ProfleHost.swift
@@ -0,0 +1,52 @@
+//
+//  ProfleHost.swift
+//  Landmarks
+//
+//  Created by Michelle Swastika on 18/04/23.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                if editMode?.wrappedValue == .active {
+                    Button("Cancel", role: .cancel) {
+                        draftProfile = modelData.profile
+                        editMode?.animation().wrappedValue = .inactive
+                    }
+                }
+                Spacer()
+                EditButton()
+            }
+            // Add a conditional view that displays either the static profile or the view for Edit mode.
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear {
+                        modelData.profile = draftProfile
+                    }
+            }
+            
+            // Read the user’s profile data from the environment to pass control of the data to the profile host.
+            ProfileSummary(profile: modelData.profile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+            .environmentObject(ModelData())
+    }
+}
